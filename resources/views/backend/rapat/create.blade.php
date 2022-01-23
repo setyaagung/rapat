3e489bdcde9b2b897ead3bf858123a2b1801a44c
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2022-01-23 16:00:23 +0700

    update footer

--- resources/views/backend/rapat/create.blade.php
+++ resources/views/backend/rapat/create.blade.php
@@ -97,7 +97,7 @@
                                         </tr>
                                     @empty
                                         <tr>
-                                            <td colspan="3" class="text-center">Tidak ada data</td>
+                                            <td colspan="5" class="text-center">Tidak ada data</td>
                                         </tr>
                                     @endforelse
                                 </tbody>
