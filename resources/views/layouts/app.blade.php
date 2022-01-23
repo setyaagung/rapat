9b07ab89c9436368b2c0bc5d51c29b7afe66f719
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2022-01-23 15:50:38 +0700

    update footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,9 @@
         .jumbotron {
             background: linear-gradient(rgba(0, 0, 0, 0.9),
                     rgba(0, 0, 0, 0.9)),
-                url(/img/pedurungan.png) no-repeat scroll;
-
+                url(/img/pedurungan.png) no-repeat center center fixed;
             background-position: center;
+            height: 500px;
         }
 
         @media (max-width: 575.98px) {
@@ -100,9 +100,12 @@
             </div>
         </nav>
 
-        <main>
+        <main style="min-height: 500px">
             @yield('content')
         </main>
+        <footer>
+            <div class="text-center bg-white pb-4">&copy; Copyright 2022</div>
+        </footer>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title','Selamat Datang')
 
 @section('content')
-    <div class="jumbotron jumbotron-fluid" style="min-height: 590px">
+    <div class="jumbotron jumbotron-fluid">
         <div class="container text-center text-white mt-5">
             <h1 class="display-3">Sistem Informasi Pemberitahuan Agenda Rapat</h1>
             <p class="lead">{{ \Carbon\Carbon::now()->isoFormat('dddd, D MMMM Y')}}</p>
