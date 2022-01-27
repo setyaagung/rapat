aa2c44554f8f30bf01de4a687f67a7b58105aaff
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2022-01-27 09:49:18 +0700

    update composer

--- resources/views/backend/pegawai/create.blade.php
+++ resources/views/backend/pegawai/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.back-main')
 
-@section('title', 'Tambah Penyelenggara')
+@section('title', 'Tambah Pegawai')
 
 @section('content')
     <div class="container-fluid">
@@ -8,7 +8,7 @@
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Tambah Penyelenggara</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Tambah Pegawai</h6>
                     </div>
                     <div class="card-body">
                         <form action="{{ route('pegawai.store')}}" method="POST">
